f66e7f3fe07a0e27c174dc70d8e120bfe3615e5f
Luciano Diaz <lucianodiazberberich@gmail.com>
2024-07-12 12:52:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const chapters = [
-        // Agregar aquí los capítulos de The Big Bang Theory
-        {season: 1, episode: 1, title: "Pilot"},
-    ];
+    let chapters = [];
+
+    function loadCSV() {
+        $.ajax({
+            url: 'big_bang_theory_episodes.csv',
+            dataType: 'text',
+        }).done(function(data) {
+            chapters = $.csv.toObjects(data);
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            console.error('Error al cargar el archivo CSV:', textStatus, errorThrown);
+        });
+    }
 
     function getRandomChapter() {
+        if (chapters.length === 0) {
+            console.error('La lista de capítulos está vacía.');
+            return;
+        }
         const randomIndex = Math.floor(Math.random() * chapters.length);
         const randomChapter = chapters[randomIndex];
         const chapterDisplay = document.getElementById("chapter-display");
-        chapterDisplay.textContent = `Temporada ${randomChapter.season}, Episodio ${randomChapter.episode}: ${randomChapter.title}`;
+        chapterDisplay.textContent = `Temporada ${randomChapter.season}, Episodio ${randomChapter.episode_num_in_season}: ${randomChapter.title}`;
     }
 
+    loadCSV();
     window.getRandomChapter = getRandomChapter;
 });
